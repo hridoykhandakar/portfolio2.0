1d6481af8e6c299ad9bae4f4508387015a299d23
Hridoy Khandakar <knhridoy150@gmail.com>
2023-08-12 10:57:43 +0600

    add favicon and title

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,3 +1,9 @@
+export const metadata = {
+  icons: {
+    icon: "/dot.svg",
+  },
+  title: "Hridoy's Portfolio",
+};
 const page = () => {
   return (
     <section className="h-screen dark:bg-gray-900 dark:text-white">
